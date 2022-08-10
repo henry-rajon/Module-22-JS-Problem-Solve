d0773e3b6f615e9070480f2f4387f4a2e419746e
Raj_Dash <rajdash3472@gmail.com>
2022-08-10 20:11:46 +0600

    splice.js

--- splice.js
+++ splice.js
@@ -1,4 +1,4 @@
 const friendsAge = [12, 21, 23, 45, 25, 26, 30, 27, 15, 29];
-const partial = friendsAge.slice(2, 5);
+const partial = friendsAge.splice(2, 3, 1222, 2222, 888);
 console.log(partial);
 console.log(friendsAge);
