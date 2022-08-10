edaabd9be2653342ae31558634d8de9b723890e4
Raj_Dash <rajdash3472@gmail.com>
2022-08-10 20:22:58 +0600

    duplicate.js file added

---
+++ duplicate.js
@@ -0,0 +1,16 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'babul', 'abul', 'ebul', 'gabul', 'babul', 'babul', 'kabul'];
+
+function removedDuplicate(names) {
+    const removedName = [];
+    for (let i = 0; i < names.length; i++){
+        const index = i;
+        const name = names[index];
+        if (removedName.includes(name) === false) {
+            removedName.push(name);
+        }
+    }
+    return removedName;
+}
+
+const getRemovedName = removedDuplicate(names);
+console.log(getRemovedName); 
